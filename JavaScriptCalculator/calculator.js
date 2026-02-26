4447329290c256a400c8e602496706b3c3d1b01e
Valentin Gardev <bestmaad@gmail.com>
2026-02-26 23:28:23 +0200

    Creating CSS
    - Adding all the visuals for the calculator;
    - Creating functions of the html file in js file;

---
+++ JavaScriptCalculator/calculator.js
@@ -0,0 +1,14 @@
+const display = document.getElementById('display'); //Getting the element ID from the html file
+
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+
+}
+
+function calculate(){
+
+}
